42f724b9fffe6c69f5529968a7baed6eb6c2d265
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-09-22 12:02:26 +0300

    changes df

--- pokemons/main.py
+++ pokemons/main.py
@@ -3,10 +3,6 @@ import pandas as pd
 # 1. load data
 df = pd.read_csv('pokemon_data.csv')
 
-# take first and last 5 records
-df_head = pd.read_csv('pokemon_data.csv').head(5)
-df_tail = pd.read_csv('pokemon_data.csv').tail(5)
-
 # read txt files
 df_text = pd.read_csv('pokemon_data.txt', delimiter='\t')
 
@@ -15,7 +11,12 @@ df_text = pd.read_csv('pokemon_data.txt', delimiter='\t')
 READING DATA IN PANDAS
 
 """
+
+
 def read_data():
+    # take first and last 5 records
+    df_head = pd.read_csv('pokemon_data.csv').head(5)
+    df_tail = pd.read_csv('pokemon_data.csv').tail(5)
     # Read Headers - column
     print(df.columns)
     # Read each Column
@@ -28,17 +29,42 @@ def read_data():
     for index, row in df.iterrows():
         print(index, row['Name'])
     # Read a specific Location(R,C) iloc + row and column
-    print(df.iloc[2,1])
+    print(df.iloc[2, 1])
     # Read wor with loc based on text
     print(df.loc[df['Type 1'] == "Fire"])
     # look at - count, mean, std, min, max
     print(df.describe())
     # sort
     print(df.sort_values('Name', ascending=False))
-    print(df.sort_values(['Type 1','HP'], ascending=[1,0]))
+    print(df.sort_values(['Type 1', 'HP'], ascending=[1, 0]))
+
 
 """
 
 CHANGES DATA
 
 """
+
+
+# Total, create new column
+def total(df):
+    df['Total'] = df['HP'] + df["Attack"] + df["Defense"] + df["Sp. Def"] + df["Speed"] + df["Sp. Atk"]
+    df['Total'] = df.iloc[:, 4:10].sum(axis=1)  # sum vertically
+    print(df.head(5))
+
+
+# Drop columns
+def drop(df):
+    df = df.drop(columns=['Total'])
+
+
+# move column Total in the middle
+def move_column(df):
+    df['Total'] = df.iloc[:, 4:10].sum(axis=1)
+    cols = list(df.columns)
+    df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
+
+    return df.iloc[:, 1:7].head(5)
+
+
+print(move_column(df))
